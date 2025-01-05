01729eea7da0b7a4a92162d30eb4dbabf2eab3c0
Samuel Morgan <samuel.edmund.morgan@icloud.com>
2025-01-05 21:09:29 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+username=$DB_USER
+password=$DB_PASSWORD
+rm /tmp/ShopDB.sql
+rm /tmp/ShopDBData.sql
+
+mysqldump -u $username -p$password ShopDB --result-file=/tmp/ShopDB.sql
+mysql -u $username -p$password ShopDBReserve < /tmp/ShopDB.sql
+
+mysqldump -u $username -p$password ShopDB --no-create-db --no-create-info --result-file=/tmp/ShopDBData.sql
+mysql -u $username -p$password ShopDBDevelopment < /tmp/ShopDBData.sql
